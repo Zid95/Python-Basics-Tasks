f41709ea8525baa54e0b1530b982c8d1c5867f3c
Zid95 <zid.abdulkarim@gmail.com>
2022-11-04 23:31:04 +0100

    2

--- 01-Variables/Tasks.py
+++ 01-Variables/Tasks.py
@@ -9,5 +9,10 @@ s = 'Mystro'
 # 5: Convert the int variable to float
 x = float(5)
 x = 5.0
-
+# 6: Can We Convert the str to int ?
+yes example:
+    x = "10" This is str
+    x = int(x)
+    print(x)
+    10
 
