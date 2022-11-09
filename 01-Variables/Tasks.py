fd6ec0f706700defd8ba7f5e49b03745dc5262fb
Zid95 <zid.abdulkarim@gmail.com>
2022-11-09 23:20:57 +0100

    finished task variables

--- 01-Variables/Tasks.py
+++ 01-Variables/Tasks.py
@@ -12,7 +12,22 @@ x = 5.0
 # 6: Can We Convert the str to int ?
 yes example:
     x = "10" This is str
-    x = int(x)
-    print(x)
+    print(int(x))
     10
-
+# 7: Create a list of numbers from 1 to 5
+y = [1,2,3,4,5]
+# 8: Create a tuple from 10 to 15
+z= (10,11,12,13,14,15)
+# 9: Convert the list to tuple
+x = [1,2,3]
+print(tuple(x))
+(1, 2, 3)
+# 10: Create a dict of 3 values
+r =  {"name": "Sami",
+  "country": "Syria",
+  "age": 27}
+# 11: Can we use semi colon ; with python
+yes
+# 12: python is interpreted or compiled ?
+interpreted
+# 13: 
