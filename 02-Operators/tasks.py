af390682ca0afbdc4d445852ca6578fea5586fec
Zid95 <zid.abdulkarim@gmail.com>
2022-11-14 23:55:12 +0100

    operators task

---
+++ 02-Operators/tasks.py
@@ -0,0 +1,25 @@
+#1: what is the difference between = and ==
+
+#2: what do we mean by using !=
+
+#3: what is the operator precedence
+
+#4: create a variable x with value of 10
+
+#5: increase x value by 15 using assignment operators
+
+#6: divide the x value by 5 using assignment operators
+
+#7: multiply the x on 3 using assignment operators
+
+#8: get module of x on 3 using assignment operators
+
+#9: using python print the module of 11 to 4
+
+
+#10:print the exponent of 2,3
+
+
+#11: divide 11 by 3 using python divison
+
+#12: can you divide 11 by 3 and get an int number
