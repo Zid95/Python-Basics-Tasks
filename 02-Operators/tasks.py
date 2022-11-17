72945b87f4363d8b09afc6d37917bd01a414ac6a
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2022-11-17 16:17:04 +0100

    Update tasks.py

--- 02-Operators/tasks.py
+++ 02-Operators/tasks.py
@@ -11,35 +11,31 @@ Operator precedence in Python simply refers to the order of operations
 #4: create a variable x with value of 10.
  x = 10
 
-
 #5: increase x value by 15 using assignment operators.
 print(x+5)
 15
+or x+=5
 #6: divide the x value by 5 using assignment operators.
 print(x/5)
 5
-
+or x/=5
 
 #7: multiply the x on 3 using assignment operators.
 print(x*3)
 15
-
+or x*=3
 #8: get module of x on 3 using assignment operators.
 print(x%3)
 0
-
+or x%=3
 
 #9: using python print the module of 11 to 4.
 print(11%4)
 3
 
 #10:print the exponent of 2,3.
-print(2**2)
-4
-
-print(3**3)
-27
-
+print(2**3)
+8
 
 #11: divide 11 by 3 using python divison
 print(11/3)
@@ -47,7 +43,5 @@ print(11/3)
 
 #12: can we divide 11 by 3 and get an int number?
 yes
-print(11%3)
-2
 print(11//3)
 3
