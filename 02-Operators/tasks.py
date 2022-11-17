204e02054aed0cc014a5717ddb614bd3b58f842e
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2022-11-17 15:52:40 +0100

    Update tasks.py

--- 02-Operators/tasks.py
+++ 02-Operators/tasks.py
@@ -1,23 +1,38 @@
 #1: what is the difference between = , ==
+# == is for value equality. It's used to know if two objects have the same value.
+#= is used for assignation of value
 
-#2: what do we mean by using !=
+#2: what do we mean by using !=?
+# not equal to operator
 
-#3: what is the operator precedence
+#3: what is the operator precedence?
+#Operator precedence in Python simply refers to the order of operations
 
-#4: create a variable x with value of 10
-
-#5: increase x value by 15 using assignment operators
-
-#6: divide the x value by 5 using assignment operators
-
-#7: multiply the x on 3 using assignment operators
-
-#8: get module of x on 3 using assignment operators
-
-#9: using python print the module of 11 to 4
+#4: create a variable x with value of 10.
+# x = 10
 
 
-#10:print the exponent of 2,3
+#5: increase x value by 15 using assignment operators.
+
+
+#6: divide the x value by 5 using assignment operators.
+
+
+
+#7: multiply the x on 3 using assignment operators.
+
+
+
+#8: get module of x on 3 using assignment operators.
+
+
+
+#9: using python print the module of 11 to 4.
+
+
+
+#10:print the exponent of 2,3.
+
 
 
 #11: divide 11 by 3 using python divison
