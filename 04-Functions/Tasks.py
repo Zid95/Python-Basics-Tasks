94b6545f3f465f8228364b9c8032b6deb9411fb0
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2022-11-18 23:50:03 +0100

    Update Tasks.py

---
+++ 04-Functions/Tasks.py
@@ -0,0 +1,31 @@
+#1:create a simple function that takes 2 numbers and print their values
+
+
+#2:create a simple function that takes 2 numbers and reurn their values
+
+
+
+#3:In the above return function, use keyword and arguments when calling the function
+
+
+
+#4:In the above return function, give x and y default values of 0 and call the
+
+
+#5: Create a function that can take any number of args and print the sum of them
+
+
+
+#6:create the same sum function using the lambda 
+
+
+
+
+#7: call lambda on the same line
+
+
+#8:difference between local variable and global variable
+
+
+
+
