a8ef72f2c82fc5106b05fe097f401f66ef9ea5e7
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2022-11-23 00:21:19 +0100

    Update Tasks.py

--- 03-Conditions/Tasks.py
+++ 03-Conditions/Tasks.py
@@ -1,26 +1,25 @@
-#1: check if 10 is bigger than 15 or not
+#1: check if 10 is bigger than 15 or not.
 
-
-#2: if 10 is not bigger than 15 print x is smaller than 15
-
-
-
-#3: in which cases we will use all
-
-
-#4: what is the difference between all, and
-
-
-#5:what is the difference between any, or
+#2: if 10 is not bigger than 15 print x is smaller than 15.
 
 
 
-#6: if we need all the conditons to be true we will use...
+#3: in which cases we will use all.
+
+
+#4: what is the difference between all, and?
+
+
+#5:what is the difference between any, or?
+
+
+
+#6: if we need all the conditons to be true we will use..
 
 
 
 
-#7:what is the difference between if, elif
+#7:what is the difference between if, elif?
 
 
 
