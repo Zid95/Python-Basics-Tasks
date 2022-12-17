b6501fcae3056dbf806d27b92fe8b766d481b06a
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2022-12-17 23:28:14 +0100

    Update Tasks.py

--- 04-Functions/Tasks.py
+++ 04-Functions/Tasks.py
@@ -17,13 +17,24 @@ print(number)
 
 
 #5: Create a function that can take any number of args and print the sum of them
+def summ(*number):
+    result = sum(number)
+    return result
 
+example:
+x = summ(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+print(x)
+output : 45
 
 
 #6:create the same sum function using the lambda 
+example :
 
+y = (lambda *number: sum(number))(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-
+print(y)
+output : 45
 
 #7: call lambda on the same line
 x = (lambda x, y: x + y)(5, 7)
